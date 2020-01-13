fee87d00031a12be97f39139257b034141ef4f6a
max114 <max.zelalem@gmail.com>
2020-01-13 12:44:54 -0500

    Nth

--- src/Matrix.java
+++ src/Matrix.java
@@ -3,11 +3,14 @@ public class Matrix {
 
 	static void findElement(int[][] matrix, int element, int rows, int cols) {
 		int i, j;
+		int Nth=0;
 		boolean exists = false;
 		for (i = 0; i < rows; i++) {
+			
 			for (j = 0; j < cols; j++) {
-				if (matrix[i][j] == element) {
-					System.out.println("found element " + element + " at column " + (i + 1) + " row " + (j + 1));
+				Nth= (Nth+1);
+				if (Nth == element) {
+					System.out.println("found element " + element + " at row " + (i + 1) + " column " + (j + 1));
 					exists = true;
 
 				}
@@ -20,13 +23,12 @@ public class Matrix {
 		}
 
 	}
-
 	public static void main(String[] args) {
 		int searchMatrix[][] = { { 4, 5, 8 },
 								 { 3, 9, 11 },
 								 { 7, 3, 6 }, };
 
-		findElement(searchMatrix, 1, 3, 3);
+		findElement(searchMatrix,3, 3, 3);
 	}
 }
 	

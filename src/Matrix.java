8375c7518ea9b625c5d9a06e127f4f1b8cea704a
max114 <max.zelalem@gmail.com>
2020-01-13 21:53:42 -0500

    indices of nth element

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,36 +1,26 @@
 
 public class Matrix {
 
-	static void findElement(int[][] matrix, int element, int rows, int cols) {
-		int i, j;
-		int Nth=0;
-		boolean exists = false;
-		for (i = 0; i < rows; i++) {
-			
-			for (j = 0; j < cols; j++) {
-				Nth= (Nth+1);
-				if (Nth == element) {
-					System.out.println("found element " + element + " at row " + (i + 1) + " column " + (j + 1));
-					exists = true;
+	static void findElement(int element, int rows, int cols) {
+		int x = element % cols;
+		int y = (element / cols) + 1;
+		int matrix = cols * rows;
 
-				}
-			}
-
-		}
-		if (exists == false) {
-			System.out.println("element " + element + " was not found in the matrix");
-			return;
+		if (x == 0) {
+			x = cols;
+			y = y - 1;
 		}
 
+		if (element > matrix) {
+			System.out.println("element is outside the matrix");
+
+		} else {
+			System.out.print("column  " + x + "  row " + y);
+		}
 	}
-	public static void main(String[] args) {
-		int searchMatrix[][] = { { 4, 5, 8 },
-								 { 3, 9, 11 },
-								 { 7, 3, 6 }, };
 
-		findElement(searchMatrix,3, 3, 3);
+	public static void main(String[] args) {
+
+		findElement(10, 3, 3);
 	}
 }
-	
-
-
